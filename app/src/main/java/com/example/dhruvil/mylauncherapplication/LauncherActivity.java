bbc6f4da78b10e64f5f2025c4cb3174a9f831f14
dhruvil <dd.droid@yahoo.com>
2015-03-25 17:14:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dhruvil/mylauncherapplication/LauncherActivity.java
@@ -0,0 +1,24 @@
+package com.example.dhruvil.mylauncherapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+      /*  Intent i = new Intent(this,HomeActivity.class);
+        startActivity(i);*/
+
+
+    }
+
+}
